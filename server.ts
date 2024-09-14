fc3993d5c360a32dfde36298af5af804235b9f41
Arman Yerkesh <a.yerkesh@gmail.com>
2024-09-14 17:30:53 +0300

    Install tsx

--- server.ts
+++ server.ts
@@ -1,22 +1,15 @@
-import express from "express";
-import type {Request, Response} from "./types.ts";
-// import {fillTemplate, updateTableTags} from "./utils/fillTemplate";
-import readFile from "./utils/fileReader.js";
-
-async function main() {
-  const res = await readFile("views/layout.html");
-  console.log(res);
-}
-main();
-
+import express, {Request, Response} from "express";
+import fs from "node:fs/promises";
+import {fillTemplate} from "./utils/fillTemplate.js";
 
 const app = express();
-const port: Number = Number(process.env.PORT) || 3030;
+const port: Number = Number(process.env["PORT"]) || 3030;
+const layout = await fs.readFile("views/layout.html", "utf-8");
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello World!');
-// })
+app.get('/', (req: Request, res: Response) => {
+  res.send(fillTemplate(layout, {}));
+});
 
-// app.listen(port, () => {
-//   console.log(`Listening on port ${port}`);
-// })
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
